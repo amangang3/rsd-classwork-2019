8d80e91cf65e1937edffc539da9447ddb3e61aee
amangang3 <amangang3@gmail.com>
2019-11-10 18:18:21 +0000

    restored to original testing file to run through problems again

--- testing/times.py
+++ testing/times.py
@@ -23,4 +23,3 @@ if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(overlap_time(large, short))
-    
